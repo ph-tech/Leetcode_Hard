b830c13a775a8ce1820630939465f352b6298635
Pragya <pragyahundia9849@gmail.com>
2026-07-06 10:05:57 +0530

    Time: 4 ms (68.84%) | Memory: 23 MB (41.77%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return next(zip(*Counter(nums).most_common(k)))
+        
