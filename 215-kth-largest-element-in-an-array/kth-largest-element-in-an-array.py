7045648865564e3dccec3831acc2ceadbc16ef40
Pragya <pragyahundia9849@gmail.com>
2026-09-06 00:02:24 +0530

    Time: 36 ms (98.37%) | Memory: 30.9 MB (70.57%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,29 +1,9 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # Base Case: If there's only one element, it must be our answer!
-        if len(nums) <= 1:
-            return nums[0]
-            
-        pivot = nums[len(nums) // 2]
+        heap = nums[:k]
+        heapq.heapify(heap)
+        for n in nums[k:]:
+            if n>heap[0]:
+                heapq.heapreplace(heap,n)
+        return heap[0]
         
-        # Keep your exact list comprehensions (using 'x' instead of 'k')
-        left  = [x for x in nums if x < pivot] 
-        mid   = [x for x in nums if x == pivot]  
-        right = [x for x in nums if x > pivot] 
-        
-        # Count how many elements are in the right and middle sections
-        len_r = len(right)
-        len_m = len(mid)
-        
-        if k <= len_r:
-            # Case 1: The k-th largest is somewhere in the 'right' (larger) side
-            return self.findKthLargest(right, k)
-            
-        elif k <= len_r + len_m:
-            # Case 2: The k-th largest falls right into the 'mid' section (the pivot)
-            return mid[0]
-            
-        else:
-            # Case 3: The k-th largest is in the 'left' (smaller) side.
-            # We subtract len_r and len_m because we are completely skipping those larger numbers!
-            return self.findKthLargest(left, k - len_r - len_m)
