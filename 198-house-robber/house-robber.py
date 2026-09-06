98185071cfa481638be8f049ef93e992b20c4213
Pragya <pragyahundia9849@gmail.com>
2026-09-06 14:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.68%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev,curr = 0,0
+        for n in nums:
+            prev,curr = curr,max(curr,prev + n)
+        return curr
+            
